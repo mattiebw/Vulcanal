b8e66312de0d7fae5fb90d177ecdcc96cb07ae77
Matt Ware <github@mattwa.re>
2025-04-03 01:10:10 +0100

    fix: Assume they removed the platform folder

--- Scripts/RunPreprocessor.sh
+++ Scripts/RunPreprocessor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo Running preprocessor...
-cd ../Tools/Preprocessor/bin/Release/net8.0/ubuntu.24.04-x64/
+cd ../Tools/Preprocessor/bin/Release/net8.0/
 ./Preprocessor "../../../../../../Content/" $1 Linux
